6eccde3957332cfa5933c01541ce57ce111782bb
Ashutosh <ashutosh.184007@gmail.com>
2026-05-26 10:01:46 +0530

    fix: remove api key

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -4,7 +4,7 @@ export const environment = {
     projectId: "lifenotes-b5261",
     appId: "1:396638903554:web:23e7a1f461e45aa398761b",
     storageBucket: "lifenotes-b5261.firebasestorage.app",
-    apiKey: "AIzaSyDnePbOFEmUvbpG8zBMu2iqIIPPoloVxZQ",
+    apiKey: "",
     authDomain: "lifenotes-b5261.firebaseapp.com",
     messagingSenderId: "396638903554",
     measurementId: "G-JESXSTEB19"
