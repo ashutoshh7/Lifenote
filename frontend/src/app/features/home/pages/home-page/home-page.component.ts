03fb63dc9518ca587d5f03b2769c4ab78b4434df
Ashutosh <ashutosh.184007@gmail.com>
2026-05-27 17:20:03 +0530

    refactor: rename from Lifenote to Momentum

--- frontend/src/app/features/home/pages/home-page/home-page.component.ts
+++ frontend/src/app/features/home/pages/home-page/home-page.component.ts
@@ -7,7 +7,7 @@ import { CommonModule } from '@angular/common';
   imports: [CommonModule],
   template: `
     <div class="p-8">
-      <h1 class="text-3xl font-bold text-gray-900 dark:text-white">Welcome to Lifenote!</h1>
+      <h1 class="text-3xl font-bold text-gray-900 dark:text-white">Welcome to Momentum!</h1>
       <p class="mt-2 text-lg text-gray-600 dark:text-gray-400">
         Select a feature from the sidebar to get started.
       </p>
